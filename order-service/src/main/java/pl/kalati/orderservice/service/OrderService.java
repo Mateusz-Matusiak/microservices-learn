1c2f4b7a7f5dad290b25d8fc3fd453b63919dc7d
Mateusz Matusiak <mateusz.matusiak@likims.com>
2023-05-03 12:42:31 +0200

    Add and configure WebClient (with dependency)

--- order-service/src/main/java/pl/kalati/orderservice/service/OrderService.java
+++ order-service/src/main/java/pl/kalati/orderservice/service/OrderService.java
@@ -3,6 +3,7 @@ package pl.kalati.orderservice.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 import pl.kalati.orderservice.dto.OrderLineItemDto;
 import pl.kalati.orderservice.dto.OrderRequest;
 import pl.kalati.orderservice.model.Order;
@@ -17,6 +18,8 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
+
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -25,8 +28,17 @@ public class OrderService {
                 .stream()
                 .map(this::mapToEntity).toList();
         order.setOrderLineItemList(orderLineItems);
-        orderRepository.save(order);
 
+        Boolean result = webClient.get()
+                .uri("http://localhost:8082/api/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+        if (Boolean.TRUE.equals(result)) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later.");
+        }
     }
 
     private OrderLineItem mapToEntity(OrderLineItemDto orderLineItemDto) {
